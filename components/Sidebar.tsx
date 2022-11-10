257e3afcefadc16c1ddffec85f60729fe3c7355b
DH <daniel@vendettistudios.com>
2022-11-10 03:32:46 -0800

    icons with padding and flex

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,12 +1,13 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { Disclosure} from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { CgClose } from "react-icons/cg";
+import { SocialIcon } from "react-social-icons"
 
 
 type Props = {}
 
-export default function Sidebar() {
+export default function Sidear() {
     return (
         <div>
             <Disclosure defaultOpen as="nav">
@@ -17,10 +18,23 @@ export default function Sidebar() {
                     />
                 </Disclosure.Button>
 
-                <Disclosure.Panel className= "md:w-full overflow-auto touch-pan-y p-6 w-1/2 h-screen bg-gray-400 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+                <Disclosure.Panel className=" lg:w-2/12 overflow-auto touch-pan-y p-6 w-full h-screen bg-gray-400 fixed top-0 -left-96 lg:left-0 peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
                     <Disclosure.Button as={Fragment}>
-                        <CgClose className="block md:hidden h-6 w-6 text-black hover:bg-gray-300 rounded-full"/>
+                        <CgClose className="block md:hidden h-6 w-6 text-black hover:bg-gray-300 rounded-full" />
                     </Disclosure.Button>
+                    <div className="">
+                        <div className="flex justify-center pt-6;">
+                            <SocialIcon
+                            url="https://linkedin.com/in/hollowaydaniel"
+                        /></div>
+                        <div className="flex justify-center pt-6">
+                            <SocialIcon
+                            url="https://github.com/vendettistudios" /></div>
+                        <div className="flex justify-center pt-6"> <SocialIcon
+                            network="email"
+                        />
+                        </div>
+                    </div>
                 </Disclosure.Panel>
             </Disclosure>
         </div>
