865cce75f9cdfcc26a443aa63c4ffba9ad7af7a2
DH <daniel@vendettistudios.com>
2022-11-12 21:08:03 -0800

    testing live

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,8 +14,9 @@ const Hero = (props: Props) => {
                 muted
                 loop
                 style={{ height: "100vh", width: "100%", objectFit: "cover" }}
-            >
-                <source src='Water.mp4' type="video/mp4" />
+                src="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-y9cdb7xgb9ciakj4.cloudflarestream.com%2F7102b82195e7169ae2da16b6023be17f%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+>
+                {/* <source src= type="video/mp4" /> */}
             </video>
             <div className="content">
                 <header className="fixed z-20 grid row-auto justify-items-center text-white text-xl lg:text-5xl">
