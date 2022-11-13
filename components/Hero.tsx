311565e654c6033bfcd7d8fb1d8735b981d6a738
DH <daniel@vendettistudios.com>
2022-11-12 21:13:47 -0800

    testing cloudflare streaming

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Stream } from "@cloudflare/stream-react";
 
 type Props = {}
 
@@ -9,15 +10,16 @@ const Hero = (props: Props) => {
             {/* <video id="videoBG" className="" autoPlay loop >
                 <source src="Water.mp4" type="video/mp4" />
             </video> */}
-            <video
+            {/* <video
                 autoPlay
                 muted
                 loop
                 style={{ height: "100vh", width: "100%", objectFit: "cover" }}
                 src="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-y9cdb7xgb9ciakj4.cloudflarestream.com%2F7102b82195e7169ae2da16b6023be17f%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
->
+> */}
                 {/* <source src= type="video/mp4" /> */}
-            </video>
+                <Stream controls src={"https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-y9cdb7xgb9ciakj4.cloudflarestream.com%2F7102b82195e7169ae2da16b6023be17f%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"}/>
+            {/* </video> */}
             <div className="content">
                 <header className="fixed z-20 grid row-auto justify-items-center text-white text-xl lg:text-5xl">
                     <h1 className="pb-4 lg:pb-6">Hello, I'm Daniel Holloway</h1>
