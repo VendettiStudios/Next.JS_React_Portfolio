23abfd4263c64e05f05df45b00c231039c7283a4
DH <daniel@vendettistudios.com>
2022-11-13 02:57:15 -0800

    improving opacity of overlay

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,37 +8,8 @@ type Props = {}
 const Hero = (props: Props) => {
     const videoID = "https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?muted=true&loop=true&autoplay=true&controls=false"
     return (
-        <div className="z-10">
-            <div className="overlay absolute h-screen opacity-30 bg-black z-20"></div>
-
-            {/* <video id="videoBG" className="" autoPlay loop >
-                <source src="Water.mp4" type="video/mp4" />
-            </video> */}
-            {/* <video
-                autoPlay
-                muted
-                loop
-                style={{ height: "100vh", width: "100%", objectFit: "cover" }}
-                src ="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?"
-                >
-            <source src="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?" type="video/mp4" />
-                </video> */}
-
-            {/* <iframe src='c'
-id="iframe"
-style={{
-    border: 0,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%"
-    }}
-        allow='autoplay; encrypted-media loop'
-        allowFullScreen
-        title='video'
-/> */}
-
+        <div className="">
+            <div className="overlay"></div>
             <div id="stream">
                 <Stream
                     src={videoID}
@@ -47,15 +18,13 @@ style={{
                     loop
                 />
             </div>
-
-
             <div className="content">
                 <header className="grid justify-items-center">
                     <span className="flex">
-                        <h1 className="pb-4 lg:pb-6 font-Comfortaa text-3xl lg:text-5xl pt-1">Hello, I'm</h1>
-                        <h1 className="font-Dancing lg:pl-4 pl-2 font-black text-3xl lg:text-5xl">Daniel Holloway</h1>
+                        <h1 className="font-Comfortaa pb-4 lg:pb-6 lg:text-5xl text-3xl pt-1">Hello, I'm</h1>
+                        <h1 className="font-Dancing font-black lg:pl-4 lg:text-5xl text-3xl pl-2">Daniel Holloway</h1>
                     </span>
-                    <h2 className="lg:pt-6 font-Comfortaa lg:text-2xl">Full-Stack Developer and Designer</h2>
+                    <h2 className="font-Comfortaa lg:pt-6 lg:text-2xl">Full-Stack Developer and Designer</h2>
                 </header>
             </div>
         </div>
