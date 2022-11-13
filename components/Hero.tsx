94ee6c61a58e0ce7721540efab40b0140a89f1bd
DH <daniel@vendettistudios.com>
2022-11-13 01:40:28 -0800

    testing live

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Stream } from "@cloudflare/stream-react";
+import ReactPlayer from "react-player"
+import { isAbsolute, relative } from 'path';
 
 type Props = {}
 
 const Hero = (props: Props) => {
+    const videoID = "https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?muted=true&loop=true&autoplay=true&controls=false"
     return (
         <div className="z-10">
             <div className="overlay absolute h-screen opacity-20 bg-black z-20"></div>
+
             {/* <video id="videoBG" className="" autoPlay loop >
                 <source src="Water.mp4" type="video/mp4" />
             </video> */}
@@ -15,11 +19,36 @@ const Hero = (props: Props) => {
                 muted
                 loop
                 style={{ height: "100vh", width: "100%", objectFit: "cover" }}
-                src="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-y9cdb7xgb9ciakj4.cloudflarestream.com%2F7102b82195e7169ae2da16b6023be17f%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
-> */}
-                {/* <source src= type="video/mp4" /> */}
-                <Stream controls src={"https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-y9cdb7xgb9ciakj4.cloudflarestream.com%2F7102b82195e7169ae2da16b6023be17f%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"}/>
-            {/* </video> */}
+                src ="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?"
+                >
+            <source src="https://customer-y9cdb7xgb9ciakj4.cloudflarestream.com/7102b82195e7169ae2da16b6023be17f/iframe?" type="video/mp4" />
+                </video> */}
+
+{/* <iframe src='c'
+id="iframe"
+style={{
+    border: 0,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+    }}
+        allow='autoplay; encrypted-media loop'
+        allowFullScreen
+        title='video'
+/> */}
+
+<div id="stream">
+      <Stream
+      src={videoID}
+      className="aspect-video"
+      autoplay
+      loop
+      />
+    </div>
+
+
             <div className="content">
                 <header className="fixed z-20 grid row-auto justify-items-center text-white text-xl lg:text-5xl">
                     <h1 className="pb-4 lg:pb-6">Hello, I'm Daniel Holloway</h1>
