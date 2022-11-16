2a542e720e88a4b97413ce828a903849288c62c7
DH <daniel@vendettistudios.com>
2022-11-15 20:34:23 -0800

    about

--- components/About.tsx
+++ components/About.tsx
@@ -9,8 +9,8 @@ const About = (props: Props) => {
             <div className="aboutContent">
                 <div className="card">
                 <h3 className="pb-6 lg:text-3xl">ABOUT</h3>
-                <div className="lg:text-xl">
-                    <p> I am a full-stack engineer with an eye for design. I like to consider myself a jack-of-all-trades, handing projects from design to deployment - digital product development, and helping debug problem points. I love traveling and my world travels have led me to a deep appreciation for expression diversity via design. I am most enthusiastic about front-end development because I enjoy challenges and how many different ways there are to go about it. Though I highly appreciate the black and white nature straight-forwardness of back-end development.
+                <div className="lg:text-xl text-black">
+                    <p> I am a full-stack engineer with an eye for design. I am a jack-of-all-trades, handling projects from design to deployment - digital product development and helping debug problem points. I love traveling, and my world travels have led me to a deep appreciation for the expression of diversity via design. I am most enthusiastic about front-end development because I enjoy challenges and how many directions you can take UX / UI. Though I highly appreciate the black-and-white nature and straightforwardness of back-end development.
                     </p>
                 </div></div>
             </div>
