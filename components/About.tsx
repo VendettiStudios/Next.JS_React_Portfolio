8540fb5e5eda7b6a384df27f64ff5949804d9e75
DH <daniel@vendettistudios.com>
2022-11-15 21:11:25 -0800

    syling

--- components/About.tsx
+++ components/About.tsx
@@ -8,13 +8,13 @@ const About = (props: Props) => {
             <div className="overlay2"></div>
             <div className="aboutContent">
                 <div className="card">
-                <h3 className="pb-6 lg:text-3xl">ABOUT</h3>
-                <div className="lg:text-xl lg:px-6">
+                <h3 className="about pb-6 lg:text-3xl">ABOUT</h3>
+                <div className="content lg:text-xl lg:px-6">
                     <p> I am a full-stack engineer with an eye for design. I consider myself a jack-of-all-trades, handling projects from design to deployment - and helping debug problem points. <br/>My world travels have led me to a deep appreciation for the expression of diversity via design. I am most enthusiastic about front-end development because I enjoy challenges and how many directions you can take UX / UI. Though I highly appreciate the black-and-white nature and straightforwardness of back-end development.
                     </p>
                 </div></div>
             </div>
-            
+
             <div className="ocean">
             <div className="wave"></div>
             <div className="wave"></div>
