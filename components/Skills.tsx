c78a6afb17e88f72709c053e6e9c315349fa0b6f
DH <daniel@vendettistudios.com>
2022-11-21 13:50:08 -0800

    content

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -17,7 +17,7 @@ const Skills = (props: Props) => {
                                         </div>
                                         <div className="label">
                                             <h2>Full-Stack Web Development and Design</h2>
-                                            <p>The stack I use mainly is the MERN stack, though I'm also proficient with several other frameworks! Below are some of the frameworks and tools I've used.</p>
+                                            <p>The stack I use mainly is the MERN stack, though I'm also proficient with several other frameworks! Below are some of the frameworks and tools I use.</p>
                                         </div>
                                     </article>
                                     <article className="timeline_entry">
@@ -25,7 +25,7 @@ const Skills = (props: Props) => {
                                         </div>
                                         <div className="label">
                                             <h2>Front End</h2>
-                                            <p>ReactJS, VanillaJS, JavaScript, JQuery, HTML5, CSS, Bootstrap, JSON, React-Bootstrap, Bulma, Tailwindcss, MUI, TypeScript, HeadlessUI</p>
+                                            <p>ReactJS, VanillaJS, JavaScript, JQuery, Progressive Web Apps, HTML5, CSS, Bootstrap, JSON, React-Bootstrap, Bulma, Tailwindcss, MUI, TypeScript, HeadlessUI</p>
                                         </div>
                                     </article>
                                     <article className="timeline_entry">
@@ -33,16 +33,15 @@ const Skills = (props: Props) => {
                                         </div>
                                         <div className="label">
                                             <h2>BackEnd</h2>
-                                            <p>MongoDB, SQL, Sequelize, APOLLO GRAPHQL, PostMan, Insmonia, Node.js, Express.js, MVC, JavaScript Computer Science, State, Redux, DNS</p>
+                                            <p>MongoDB, SQL - Sequelize, GraphQL, PostMan, Insmonia, Node.js, Express.js, MVC, State, Redux, DNS, API Integration, Object-Oriented-Programming</p>
                                         </div>
                                     </article>
                                     <article>
                                         <div className="timeline_icon timeline_icon_4" >
                                         </div>
                                         <div className="label">
-                                            <h2>Summary<span> The Wrap! + Other Complimentary Skills</span></h2>
-                                            <p> MERN - Web API's - Object Oriented Programming - Object Oriented Mapping - Progressive Web Applications - GitHub - UI/UX DESIGN - Project Management - Team Coordination - Time Management - Communication
-                                            </p>
+                                            <h2>UX/UI Tools & Deployment Tools</h2>
+                                            <p>Figma, Github, CloudFlare, Vercel, Heroku, EC2</p>
                                         </div>
                                     </article>
                                 </div>
