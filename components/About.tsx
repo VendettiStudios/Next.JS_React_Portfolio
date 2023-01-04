dca4fe2a957c9d3646e713136c9446fc89db09e8
DH <daniel@vendettistudios.com>
2023-01-04 13:53:47 -0800

    About

--- components/About.tsx
+++ components/About.tsx
@@ -8,16 +8,18 @@ const About = (props: Props) => {
             <div className="overlay2"></div>
             <div className="aboutContent">
                 <div className="card">
-                <h3 className="about pb-6 lg:text-3xl">ABOUT</h3>
-                <div className="content lg:text-xl lg:px-6">
-                    <p> I am a full-stack engineer with an eye for design. I consider myself a jack-of-all-trades, handling projects from design to deployment - and helping debug problem points. <br/>My world travels have led me to a deep appreciation for the expression of diversity via design. I am most enthusiastic about front-end development because I enjoy challenges and how many directions you can take UX / UI. Though I highly appreciate the black-and-white nature and straightforwardness of back-end development.
-                    </p>
-                </div></div>
+                    <h3 className="about pb-6 lg:text-3xl">ABOUT</h3>
+                    <div className="content lg:text-xl lg:px-6">
+                        <p> As a full-stack engineer with a passion for design, I have the skills and experience to take projects from conception to deployment. I enjoy tackling challenges and exploring the many possibilities of user experience and user interface design, particularly in front-end development. However, I also value the structured approach of back-end development.
+                        <br />
+                            In addition, my travels and exposure to diverse cultures have given me a deep appreciation for the power of design to express individuality and diversity. I am excited to bring my skills and perspective to new challenges and opportunities.
+                        </p>
+                    </div></div>
             </div>
 
             <div className="ocean">
-            <div className="wave"></div>
-            <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
             </div>
         </div>
     )
