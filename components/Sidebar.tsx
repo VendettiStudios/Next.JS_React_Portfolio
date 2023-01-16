48897ca9dd8f4e6c98c9faf12942653cdeb1247d
DH <daniel@vendettistudios.com>
2023-01-16 00:31:16 -0800

    background circles broken, Sidebar build begun

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 import BackgroundCircles from './BackgroundCircles'
 import Link from 'next/link'
 import { Spiral as Hamburger } from 'hamburger-react'
-import { GiHamburgerMenu } from "react-icons/gi";
+
 type Props = {}
 
 export default function Sidebar() {
